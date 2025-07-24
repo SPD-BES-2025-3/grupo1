7e8442e7cc21865c54fc3878f267417674dfdcd5
Matheus Lucas <nerdola.cartola@gmail.com>
2025-07-24 17:26:35 -0300

    Add docker configuration

--- src/app/config.py
+++ src/app/config.py
@@ -1,5 +1,8 @@
 #Aqui ficam as configurações e variáveis de ambiente da app
+import os
 
-MONGO_URI = "mongodb://localhost:27017/"
+# Use localhost when running server locally, mongodb when running in Docker
+MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
+MONGO_URI = f"mongodb://{MONGO_HOST}:27017/"
 MONGO_DB_NAME = "articles_db"
 CHROMA_DB_PATH = "./chroma_db"
