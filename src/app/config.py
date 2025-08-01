9ef335c8d4cc7b13a0b9688a7590b55af14dac11
Luís Henrique <luis_alves@discente.ufg.br>
2025-07-31 22:35:36 -0300

    update

--- src/app/config.py
+++
@@ -1,8 +0,0 @@
-#Aqui ficam as configurações e variáveis de ambiente da app
-import os
-
-# Use localhost when running server locally, mongodb when running in Docker
-MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
-MONGO_URI = f"mongodb://{MONGO_HOST}:27017/"
-MONGO_DB_NAME = "articles_db"
-CHROMA_DB_PATH = "./chroma_db"
